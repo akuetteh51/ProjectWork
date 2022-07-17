020000b0d1869cfd30d2af418c54e86769b0f110
akuetteh51 <akuetteh58@gmail.com>
2022-07-17 18:00:29 +0000

    modile app added and Splash Screen Created

---
+++ Testing/vita_check/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:vita_check/views/splash.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Splash_screen(),
+    ),
+  );
+}
---
+++ Testing/vita_check/lib/views/splash.dart
@@ -0,0 +1,19 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class Splash_screen extends StatelessWidget {
+  const Splash_screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.asset(
+        fit: BoxFit.cover,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        "assets/splash.png",
+      ),
+    );
+  }
+}
